29aca7414527292e07727fd2e533f36c4ab741c2
Aynzrhp <aynazrahmniparhizkar@gmail.com>
2023-11-08 11:12:40 +0330

    Updated

--- second.py
+++ second.py
@@ -1,1 +1,10 @@
-def 
+def combination(n,r):
+    if r == 0 or r == n:
+        return 1;
+    else:
+        return combination(n-1,r-1)+combination(n-1,r)
+
+n = int(input("n:"))
+r = int(input("r:"))
+result= combination(n,r)
+print(f"Combination of ({n},{r}) is {result}")
