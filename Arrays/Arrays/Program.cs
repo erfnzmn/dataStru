dc535f4d120b7186f1e845692005143274f3c744
erfnzmn <fanzm1316@gmail.com>
2023-11-15 10:24:49 +0330

    array added find

--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -2,38 +2,27 @@
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-        int[] ArrayList = new int[10];
-        ArrayList[0] = 2;
-        ArrayList[1] = 9;
-        ArrayList[2] = 0;
-        ArrayList[3] = 15;
-        ArrayList[4] = 5;
-        int x = 12;
-        int n = 5;
-        int pos = 3;
 
-        Console.Write("Before Insertion: ");
-        for (int i = 0; i < n; i++)
-        {
-            Console.Write(ArrayList[i] + " ");
-        }
-        Console.WriteLine();
-        Insert(ArrayList, n, x, pos);
-        n += 1;
-        Console.Write("After Insertion: ");
-        for (int i = 0; i < n; i++)
-            Console.Write(ArrayList[i] + " ");
-        Console.ReadKey();
-    }
-    static int Insert(int[] arr, int n, int x, int pos)
+    static int Insert(int[] array, int n, int x, int pos)
     {
         {
             for (int i = n - 1; i >= pos; i--)
-                arr[i + 1] = arr[i];
-            return arr[pos] = x;
+                array[i + 1] = array[i];
+            return array[pos] = x;
         }
+    }
+    static int Find(int[] array, int b, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i] == b)
+            {
+                return i;
+            }
+            else
+                return -1;
+        }
+        return -2;
     }
 
     static int Delete(int[] array, int n, int x)
@@ -42,12 +31,9 @@ internal class Program
 
         if (pos == -1)
         {
-            Console.WriteLine("Element not found");
-            return n;
+            return -1;
         }
-
         else
-
             for (int i = pos; i < n - 1; i++)
                 array[i] = array[i + 1];
 
