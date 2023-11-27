623a4d57c419f292235f614e79eddde476f6f901
Aynzrhp <aynazrahmniparhizkar@gmail.com>
2023-11-27 15:18:53 +0330

    Changing

--- Queue with stack/Queue with stack/Program.cs
+++ Queue with stack/Queue with stack/Program.cs
@@ -7,24 +7,24 @@ internal class Program
 
     class Queue
     {
-        private Stack<int> Q1stack=new Stack<int>();
+        private Stack<int> Q1stack = new Stack<int>();
         private Stack<int> Q2stack = new Stack<int>();
-       
+
         public void addQueue(int item)
         {
-            while(Q1stack.Count>0)
+            while (Q1stack.Count > 0)
             {
                 Q2stack.Push(Q1stack.Pop());
             }
             Q1stack.Push(item);
-            while(Q2stack.Count>0)
+            while (Q2stack.Count > 0)
             {
                 Q1stack.Push(Q2stack.Pop());
             }
         }
         public int Delete()
         {
-            if(Q1stack.Count==0)
+            if (Q1stack.Count == 0)
             {
                 return 0;
             }
@@ -33,16 +33,4 @@ internal class Program
             return item;
         }
     }
-
-    //private static void Main(string[] args)
-    //{
-    //    Queue Q = new Queue();
-    //    Q.addQueue(1);
-    //    Q.addQueue(2);
-    //    Q.addQueue(4);
-    //    Q.addQueue(7);
-    //    Q.addQueue(9);
-       
-    //    Console.ReadKey();
-    //}
 }
