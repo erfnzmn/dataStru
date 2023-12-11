52492639f2f6fe5d006783471c55ec186c71e3e0
Aynzrhp <aynazrahmniparhizkar@gmail.com>
2023-12-11 21:51:00 +0330

    StackWithLinkedlist,Queue with Linkedlist

---
+++ QueueLL/QueueLL/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QueueLL.LinkedList;
+internal class Program
+{
+    class Queue
+    {
+    public Node front;
+    public Node rear;
+    public void enqueue(int item)
+    {
+        if (front == rear)
+        {
+            return;
+        }
+        else
+        {
+            rear.InsertAtTheEnd(item);
+        }
+    }
+    public void dequeue()
+    {
+        if (front == null)
+        {
+            return;
+        }
+        else
+        {
+            front.DeleteFromBeginning();
+        }
+    }
+        public void printlist()
+        {
+            printlist();
+        }
+        
+}
+    private static void Main(string[] args)
+    {
+        Queue myque = new Queue();
+        myque.enqueue(1);
+        myque.enqueue(8);
+        myque.enqueue(34);
+        myque.printlist();
+
+    }
+}
---
+++ StackLL/StackLL/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static StackLL.LinkedList;
+
+internal class Program
+{
+    private Node top;
+
+    public bool isempty()
+    {
+        if (top.head == null)
+            return true;
+        else
+            return false;
+    }
+    public void push(int item)
+    {
+        if (isempty())
+        {
+            return;
+        }
+        else
+        {
+            top.InsertAtTheEnd(item);
+        }
+    }
+    public void pop()
+    {
+        top.DeleteFromEnd();
+    }
+   
+}
