8fba794d79e7ae1578df649823f253a82832815c
erfnzmn <fanzm1316@gmail.com>
2023-12-28 09:47:38 +0330

    added Hashtable

---
+++ HashTableProject/HashTableProject/HashT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProject
+{
+    class HashT
+    {
+        public Node[] Hash(int[] A)
+        {
+            Node[] Hashtable = new Node[10];
+            for (int i = 0; i < 10; i++)
+                Hashtable[i] = new Node(10);
+            int[] Count = new int[10];
+            for (int i = 0; i < A.Length; i++)
+            {
+                int place = A[i] % 10;
+
+                Hashtable[place].InsertAtTheEnd(A[i]);
+            }
+            return Hashtable;
+        }
+        public void printHashT(Node[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+                A[i].printList();
+        }
+    }
+}
---
+++ HashTableProject/HashTableProject/Node.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProject
+{
+    public class Node
+    {
+
+        public int data;
+        public Node link;
+        Node head;
+        public Node(int x)
+        {
+            data = x;
+            link = null;
+        }
+        public void InsertAtTheEnd(int ndata)
+        {
+            Node newnode = new Node(ndata);
+            if (head == null)
+            {
+                head = new Node(ndata);
+                return;
+            }
+            newnode.link = null;
+
+            Node last = head;
+            while (last.link != null)
+                last = last.link;
+
+            last.link = newnode;
+            return;
+        }
+        public void printList()
+        {
+            Node node = head;
+            while (node != null)
+            {
+                Console.Write(node.data + " ");
+                node = node.link;
+            }
+        }
+    }
+}
